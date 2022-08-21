890e71143a4a429e1a952a1a051cc77ebe36aa40
WonTaeJang <jwt0212@gmail.com>
2022-08-21 22:00:00 +0900

    pinia

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
+import {createPinia} from 'pinia'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
 
-loadFonts()
+const pinia = createPinia();
+const app = createApp(App);
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+app.use(pinia);
+app.mount('#app');
