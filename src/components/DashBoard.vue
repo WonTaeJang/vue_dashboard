105e26515866442a0c93df1b6f3fe7fe3e878841
WonTaeJang <jwt0212@gmail.com>
2022-08-24 23:39:26 +0900

    element 제어

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -14,6 +14,14 @@
       {{listStore.getDataAll}}
     </div>
     
+    <div>
+      <h2>DashBoard</h2>
+
+      <div>
+        <label @click="clickLabel">{{current}}</label>
+      </div>
+
+    </div>
 
   </div>
 </template>
@@ -24,12 +32,19 @@ import {ref, onMounted, computed} from 'vue'
 
 const cnt = ref(0);
 const city = ref('');
+const current = ref('sss');
 
 const listStore = useListStore();
 
 
 function plus(){
     cnt.value++;
+}
+
+function clickLabel(){
+  current.value += 'L';
+    //console.log(event.target.innerText);
+    // console.log(event);
 }
 
 onMounted(()=>{
